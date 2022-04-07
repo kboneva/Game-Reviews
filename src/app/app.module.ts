5b5ac73470879b14515e29bc51828796dea5bc36
Krisi Boneva <krisyyy98@gmail.com>
2022-04-08 02:05:55 +0300

    Generated basic modules and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,29 @@ import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { GamesListComponent } from './feature/games-list/games-list.component';
+import { GamesListItemComponent } from './feature/games-list-item/games-list-item.component';
+import { GameComponent } from './feature/game/game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GamesListComponent,
+    GamesListItemComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    CoreModule,
+    SharedModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
-    provideDatabase(() => getDatabase())
+    provideDatabase(() => getDatabase()),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/feature/games-list-item/games-list-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-games-list-item',
+  templateUrl: './games-list-item.component.html',
+  styleUrls: ['./games-list-item.component.css']
+})
+export class GamesListItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
