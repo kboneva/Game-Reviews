027e870d4497c70d0a2aba30d904dd25ecadeb7a
Krisi Boneva <krisyyy98@gmail.com>
2022-04-07 23:11:12 +0300

    Creating project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'gamereviews-8c508',
+    appId: '1:251756304688:web:51b7feba907a2c756310f5',
+    databaseURL: 'https://gamereviews-8c508-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'gamereviews-8c508.appspot.com',
+    apiKey: 'AIzaSyC_rm8rNUVAi9Vj6uQan0uIDPunCXPh7JI',
+    authDomain: 'gamereviews-8c508.firebaseapp.com',
+    messagingSenderId: '251756304688',
+  },
   production: true
 };
